097ff8762d0e92d22302e5634af97363cef61018
Soroush Karimi <starliner.dev.app@gmail.com>
2020-04-04 17:54:10 -0500

    Update index

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 require('dotenv').config()
-
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+const SERVICES = [
+    {
+        name: 'SPOTIFY',
+        format: 'https://open.spotify.com/'
+    },
+    {
+        name: 'APPLE_MUSIC',
+        format: 'https://music.apple.com/'
+    }
+]
+
+const recognize_link = (link) => {
+    let type;
+    SERVICES.map(item => {
+        if(link.includes(item.format)) {
+            type = item.name
+        }
+    })
+    return type
+}
+
+const bot = new Telegraf(process.env.BOT_KEY)
+bot.start((ctx) => ctx.reply('Welcome to Music Bridge Bot.'))
+bot.help((ctx) => ctx.reply('Send me a link from Spotify or Apple music and I can find the other one for you.'))
+bot.use((ctx) => {
+    const message_text = ctx.message.text
+    const link_type = recognize_link(message_text)
+    if (link_type === undefined) {
+        ctx.reply("Link is not valid. Please try a correct link.")
+    } else {
+        ctx.reply(link_type)
+        // const result = translate_link(link_type, message_text)
+    }
+    console.log(link_type)
+  })
 bot.launch()
