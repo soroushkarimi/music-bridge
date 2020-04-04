84c2951fd2eecc139a775c169421b839b0c62d96
Soroush Karimi <starliner.dev.app@gmail.com>
2020-04-04 15:01:59 -0500

    Add dotenv package

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
